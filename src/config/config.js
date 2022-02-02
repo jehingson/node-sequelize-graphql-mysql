c109bee7a1f55512f315cc10efd89cd77a46f29b
jehingson <jehingson25@gmail.com>
2022-02-01 20:11:08 -0500

    [fix] resolvers

---
+++ src/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "development": {
+        "username": "u6tzupfsbcmkxh9x",
+        "password": "x2wNbgS9EZJmY0gKYRKU",
+        "database": "bxf9ap8d2r7tvtvgavpo",
+        "host": "bxf9ap8d2r7tvtvgavpo-mysql.services.clever-cloud.com",
+        "dialect": "mysql"
+      },
+      "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+      },
+      "production": {
+        "username": "root",
+        "password": null,
+        "database": "database_production",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+      }
+}
